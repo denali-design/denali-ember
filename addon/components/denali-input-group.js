c34bab7d0d870d0c631f78200f88e62a5355f8c0
Chris Williamson <christopher.e.williamson@gmail.com>
2020-05-29 16:00:24 -0500

    feat: DenaliInputGroup

---
+++ addon/components/denali-input-group.js
@@ -0,0 +1,29 @@
+import Component from '@glimmer/component';
+import { arg } from 'ember-arg-types';
+import { string, boolean } from 'prop-types';
+
+export default class DenaliInputGroupComponent extends Component {
+  @arg(string)
+  label;
+
+  @arg(boolean)
+  isStacked;
+
+  @arg(boolean)
+  isResponsive;
+
+  @arg(boolean)
+  autoWidth;
+
+  get isStackedClass() {
+    return this.isStacked ? 'is-stacked' : undefined;
+  }
+
+  get isResponsiveClass() {
+    return this.isResponsive ? 'responsive' : undefined;
+  }
+
+  get autoWidthClass() {
+    return this.autoWidth ? 'auto' : undefined;
+  }
+}
