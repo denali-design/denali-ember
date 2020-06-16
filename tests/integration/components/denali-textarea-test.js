eeca671525512b4c08b0e5d4d7f66d92523da605
Jana Holderbaugh <holderb5@msu.edu>
2020-06-16 15:31:21 -0500

    fix: removed settled from import statement in textarea test

--- tests/integration/components/denali-textarea-test.js
+++ tests/integration/components/denali-textarea-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { fillIn, render, settled } from '@ember/test-helpers';
+import { fillIn, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | denali-textarea', function (hooks) {
