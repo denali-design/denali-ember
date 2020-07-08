b35196eb529fb0dab5d2915d5f7dd09d55ae7c41
Jana Holderbaugh <holderb5@msu.edu>
2020-07-08 09:28:34 -0500

    fix: add copyrights to new files

--- addon/components/denali-table/th.js
+++ addon/components/denali-table/th.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2020, Verizon Media
+ * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
+ */
+
 import Component from '@glimmer/component';
 import { arg } from 'ember-arg-types';
 import { oneOf } from 'prop-types';
