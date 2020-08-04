76ef6f5c76697da76faa21c82acd9fd87341cc24
Alex Aralis <alex.aralis@verizonmedia.com>
2020-08-04 14:33:18 -0500

    feat: add storybook knobs, add core-js, remove storybook docs, move denali button enums

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,6 @@ module.exports = {
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-storysource',
-    '@storybook/addon-docs',
+    '@storybook/addon-knobs/register',
   ],
 };
--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -1,17 +1,36 @@
 import { hbs } from 'ember-cli-htmlbars';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
+import { STYLES, SIZES, TYPES } from '../addon/components/denali-button-enums';
 
 export default {
   title: 'DenaliButton',
+  component: 'DenaliButton',
+  decorators: [withKnobs],
 };
 
 export const Solid = () => ({
   template: hbs`
-    <DenaliButton>Default</DenaliButton>
-    <DenaliButton @isActive={{true}}>Active</DenaliButton>
-    <DenaliButton disabled={{true}}>Disabled</DenaliButton>
+    <DenaliButton
+      @isActive={{active}}
+      @isInverse={{inverse}}
+      @size={{size}}
+      @style={{style}}
+      @type={{type}}
+      disabled={{disabled}}
+      onClick={{onClick}}
+    >
+      {{text}}
+    </DenaliButton>
   `,
   context: {
+    active: boolean('Active', false),
+    disabled: boolean('Disabled', false),
+    inverse: boolean('Inverse', false),
     onClick: action('clicked'),
+    size: select('Size', SIZES),
+    style: select('Style', STYLES),
+    text: text('Button Text', 'Button'),
+    type: select('Type', TYPES),
   },
 });
