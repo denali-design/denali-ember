827f209c1fac5746bc9831cf88b1ae4dca408c16
Jon Kilroy <kilroy@verizonmedia.com>
2021-01-12 22:50:54 -0600

    feat: add ember-power-select multi-select styles

---
+++ stories/ember-power-select/power-select-multiple.stories.js
@@ -0,0 +1,54 @@
+import { hbs } from 'ember-cli-htmlbars';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, array } from '@storybook/addon-knobs';
+import { example } from '../knob-categories';
+
+export default {
+  title: 'Ember Power Select/PowerSelectMultiple',
+  component: 'PowerSelectMultiple',
+  decorators: [withKnobs],
+};
+
+const selected = { name: 'Denali' };
+export const Default = () => ({
+  template: hbs`
+    <PowerSelectMultiple
+      @searchEnabled={{true}}
+      @searchField="name"
+      @options={{items}}
+      @selected={{selectedItems}}
+      @placeholder="Select Some Items..."
+      @onChange={{queue onChange (fn (mut selectedItems))}}
+      @renderInPlace={{true}}
+      as |item|
+    >
+      {{item.name}}
+    </PowerSelectMultiple>
+  `,
+  context: {
+    items: [selected, { name: 'Styled', disabled: true }, { name: 'Power' }, { name: 'Select' }, { name: 'Multiple' }],
+    selectedItems: [selected],
+    onChange: action('onChange'),
+  },
+});
+
+export const Playground = () => ({
+  template: hbs`
+    <PowerSelectMultiple
+      @searchEnabled={{true}}
+      @options={{items}}
+      @selected={{selectedItems}}
+      @placeholder="Select Some Items..."
+      @onChange={{queue onChange (fn (mut selectedItems))}}
+      @renderInPlace={{true}}
+      as |name|
+    >
+      {{name}}
+    </PowerSelectMultiple>
+  `,
+  context: {
+    items: array('items', ['Denali', 'Styled', 'Power', 'Select', 'Multiple'], ',', example),
+    selectedItems: array('selectedItems', ['Denali'], ',', example),
+    onChange: action('onChange'),
+  },
+});
