681b35069eab9847426deb7d2bf56acff528c838
Jon Kilroy <jkilroy@gmail.com>
2021-01-13 16:39:14 -0800

    docs: add denali-text-area story doc (#58)

--- addon/components/denali-text-area.js
+++ addon/components/denali-text-area.js
@@ -1,16 +1,18 @@
 /**
- * Copyright 2020, Verizon Media
+ * Copyright 2021, Verizon Media
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  */
 import Component from '@glimmer/component';
 import { arg } from 'ember-arg-types';
 import { oneOf, string } from 'prop-types';
-
-const states = ['active', 'error'];
+import { STATES } from './denali-text-area-enums';
 
 export default class DenaliTextAreaComponent extends Component {
-  @arg(oneOf(states)) state;
-  @arg(string) errorMsg;
+  @arg(oneOf(STATES))
+  state;
+
+  @arg(string)
+  errorMsg;
 
   get activeClass() {
     return this.state === 'active' ? 'is-active' : undefined;
