92ee6078e0709a93f4c8fda6ae2b2709a71b8fa2
Chris Williamson <christopher.e.williamson@gmail.com>
2021-02-10 17:24:26 -0600

    feat(denali-navbar/icon): updated to support name (#68)
    feat(denali-navbar/icon): support navbar icon name

--- tests/integration/components/denali-navbar/icon-test.js
+++ tests/integration/components/denali-navbar/icon-test.js
@@ -18,4 +18,18 @@ module('Integration | Component | denali-navbar/icon', function (hooks) {
     this.set('icon', 'bug');
     assert.dom('.nav-icon .d-icon.d-bug').exists('DenaliNavbar::Icon exists and has the correct icon class');
   });
+
+  test('name renders', async function (assert) {
+    await render(hbs`
+      <DenaliNavbar::Icon
+        @icon="code"
+        @name={{this.name}}
+      />
+    `);
+    assert.dom('.nav-icon .d-icon.d-code').exists('DenaliNavbar::Icon exists and has the correct icon class');
+    assert.dom('.icon-name').doesNotExist('DenaliNavbar::Icon exists and does not have a name.');
+
+    this.set('name', 'My Icon');
+    assert.dom('.icon-name').hasText('My Icon', 'DenaliNavbar::Icon exists and has a name.');
+  });
 });
