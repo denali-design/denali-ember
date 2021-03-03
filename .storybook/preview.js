9d9c0083e301f3397f8a899e793af7de88e33595
Jon Kilroy <kilroy@verizonmedia.com>
2021-03-03 16:05:41 -0600

    docs: add denali theme to storybook docs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,32 @@
+import './style.css';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  docs: { page: null },
+  html: {
+    prettier: {
+      tabWidth: 4,
+      useTabs: false,
+      htmlWhitespaceSensitivity: 'strict',
+    },
+  },
+  options: {
+    storySort: {
+      method: 'alphabetical',
+      order: [],
+    },
+  },
+};
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      // array of plain string values or MenuItem shape (see below)
+      items: ['light', 'dark'],
+    },
+  },
 };
