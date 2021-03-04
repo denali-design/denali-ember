cc66b58c19112db007230f7abfb9738dd05368d5
Chris Williamson <christopher.e.williamson@gmail.com>
2021-03-04 13:29:27 -0600

    docs(navbar): added additional substories based on denali storybook

--- stories/navbar.stories.js
+++ stories/navbar.stories.js
@@ -41,6 +41,138 @@ export const Default = () => ({
     </Nav.Right>
   </DenaliNavbar>
   `,
+});
+
+export const Items = () => ({
+  template: hbs`
+  <DenaliNavbar
+        @isResponsive={{isResponsive}}
+        @isMenuActive={{isMenuActive}}
+        as |Nav| >
+    <Nav.Left as |Section|>
+      <Section.Logo src="https://denali-design.github.io/denali-css/denali-logo.svg"/>
+    </Nav.Left>
+    <Nav.Right as |Section|>
+      <Section.Item @isActive={{true}}>
+        Home
+      </Section.Item>
+      <Section.Item>
+        About
+      </Section.Item>
+      <Section.Item>
+        Contact
+      </Section.Item>
+    </Nav.Right>
+  </DenaliNavbar>
+  `,
+});
+
+export const Icons = () => ({
+  template: hbs`
+  <DenaliNavbar
+        @isResponsive={{isResponsive}}
+        @isMenuActive={{isMenuActive}}
+        as |Nav| >
+    <Nav.Left as |Section|>
+      <Section.Logo src="https://denali-design.github.io/denali-css/denali-logo.svg"/>
+    </Nav.Left>
+    <Nav.Right as |Section|>
+      <Section.Icon
+        @icon="dashboard"
+      />
+      <Section.Icon
+        @icon="menu-dialpad"
+      />
+      <Section.Icon
+        @icon="user-profile-circle"
+      />
+    </Nav.Right>
+  </DenaliNavbar>
+  `,
+});
+
+export const Left = () => ({
+  template: hbs`
+  <DenaliNavbar
+        @isResponsive={{isResponsive}}
+        @isMenuActive={{isMenuActive}}
+        as |Nav| >
+    <Nav.Left as |Section|>
+      <Section.Logo src="https://denali-design.github.io/denali-css/denali-logo.svg"/>
+    </Nav.Left>
+  </DenaliNavbar>
+  `,
+});
+
+export const Center = () => ({
+  template: hbs`
+  <DenaliNavbar
+        @isResponsive={{isResponsive}}
+        @isMenuActive={{isMenuActive}}
+        as |Nav| >
+    <Nav.Left as |Section|>
+      <Section.Logo src="https://denali-design.github.io/denali-css/denali-logo.svg"/>
+    </Nav.Left>
+    <Nav.Center as |Section|>
+      <Section.Item>
+        Center
+      </Section.Item>
+    </Nav.Center>
+  </DenaliNavbar>
+  `,
+});
+
+export const Right = () => ({
+  template: hbs`
+  <DenaliNavbar
+        @isResponsive={{isResponsive}}
+        @isMenuActive={{isMenuActive}}
+        as |Nav| >
+    <Nav.Left as |Section|>
+      <Section.Logo src="https://denali-design.github.io/denali-css/denali-logo.svg"/>
+    </Nav.Left>
+    <Nav.Right as |Section|>
+      <Section.Icon
+        @icon="user-profile-circle"
+      />
+    </Nav.Right>
+  </DenaliNavbar>
+  `,
+});
+
+export const Playground = () => ({
+  template: hbs`
+  <DenaliNavbar
+        @isResponsive={{isResponsive}}
+        @isMenuActive={{isMenuActive}}
+        as |Nav| >
+    <Nav.Left as |Section|>
+      <Section.Logo src="https://denali-design.github.io/denali-css/denali-logo.svg"/>
+    </Nav.Left>
+    <Nav.Center as |Section|>
+      <Section.Item @isActive={{true}}>
+        Home
+      </Section.Item>
+      <Section.Item>
+        About
+      </Section.Item>
+      <Section.Item>
+        Contact
+      </Section.Item>
+    </Nav.Center>
+    <Nav.Right as |Section|>
+      <Section.Icon
+        @icon="dashboard"
+      />
+      <Section.Icon
+        @icon="menu-dialpad"
+      />
+      <Section.Icon
+        @icon="user-profile-circle"
+      />
+    </Nav.Right>
+  </DenaliNavbar>
+  `,
   context: {
     isResponsive: boolean('isResponsive', true, argument),
     isMenuActive: boolean('isMenuActive', true, argument),
