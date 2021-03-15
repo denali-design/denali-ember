a50570ad0b8d879b29a44c12c58e59662da1682b
Utsav Poudel <utsab.poudel@verizonmedia.com>
2021-03-15 13:39:22 -0400

    refactor: removed line in denali-progress-enums

--- addon/components/denali-progress-enums.js
+++ addon/components/denali-progress-enums.js
@@ -2,5 +2,4 @@
  * Copyright 2021, Verizon Media
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  */
-
 export const SIZES = ['default', 'small', 'medium', 'large'];
