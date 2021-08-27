8f89c0ff0f81866ec86c271592b50114cae0d53b
jkusa <jkilroy@gmail.com>
2021-08-27 13:57:38 -0700

    refactor: don't render menu content when inactive

--- addon/components/denali-menu.js
+++ addon/components/denali-menu.js
@@ -1,16 +1,29 @@
 /**
- * Copyright 2020, Verizon Media
+ * Copyright 2021, Verizon Media
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  */
 import Component from '@glimmer/component';
 import { arg } from 'ember-arg-types';
 import { oneOf } from 'prop-types';
+import { tracked } from '@glimmer/tracking';
 import { ALIGNMENTS } from './denali-menu-enums';
+import { action } from '@ember/object';
 
 export default class DenaliMenuComponent extends Component {
+  @tracked isActive = false;
+
   @arg(oneOf(ALIGNMENTS)) alignContent;
 
   get alignContentClass() {
     return this.alignContent ? `is-${this.alignContent}` : undefined;
+  }
+
+  get isActiveClass() {
+    return this.isActive ? 'is-active' : undefined;
+  }
+
+  @action
+  toggleIsActive() {
+    this.isActive = !this.isActive;
   }
 }
